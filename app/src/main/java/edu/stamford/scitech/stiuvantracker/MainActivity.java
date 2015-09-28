f906fe001c33a5c1e5a1a8b614aef45ec06087ec
xniko <xnikokallio@gmail.com>
2015-09-28 10:14:51 +0700

    Updated UI

--- app/src/main/java/edu/stamford/scitech/stiuvantracker/MainActivity.java
+++ app/src/main/java/edu/stamford/scitech/stiuvantracker/MainActivity.java
@@ -1,10 +1,106 @@
 package edu.stamford.scitech.stiuvantracker;
 
-import android.app.Activity;
+
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set a toolbar which will replace the action bar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Setup the viewPager
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        MyPagerAdapter pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        // Setup the Tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        // By using this method the tabs will be populated according to viewPager's count and
+        // with the name from the pagerAdapter getPageTitle()
+        tabLayout.setTabsFromPagerAdapter(pagerAdapter);
+        // This method ensures that tab selection events update the ViewPager and page changes update the selected tab.
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private class MyPagerAdapter extends FragmentStatePagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            switch(pos) {
+
+                case 0: return Fragment1.newInstance();
+                case 1: return Fragment2.newInstance();
+                case 2: return Fragment3.newInstance();
+                default: return Fragment1.newInstance();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            String tabName = "";
+            switch(position) {
+                case 0: tabName = "Schedule"; break;
+                case 1: tabName = "Map"; break;
+                case 2: tabName = "Settings"; break;
+            }
+           // return "Tab " + (position + 1);
+            return tabName;
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_about) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
+
+/*
 
 public class MainActivity extends Activity {
 
@@ -36,3 +132,4 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 }
+*/
