ea035db14bb3bc74f2161ebdc9c3794deaddd338
Thakorn <thakorn.m@stamford.edu>
2015-10-05 09:31:37 +0700

    Update Map tab and schedule

--- app/src/main/java/edu/stamford/scitech/stiuvantracker/fragment1.java
+++ app/src/main/java/edu/stamford/scitech/stiuvantracker/fragment1.java
@@ -2,14 +2,21 @@ package edu.stamford.scitech.stiuvantracker;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class fragment1 extends Fragment {
+
+    ListView lv;
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -29,10 +36,64 @@ public class fragment1 extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fragment1, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        lv = (ListView) getActivity().findViewById(R.id.lv);
+
+        String[] values = new String[] {
+                "Airport Link",
+                "MRT",
+                "Bansuan",
+                "Stamford",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder",
+                "Schedule Item Placeholder"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // ListView Clicked item index
+                int itemPosition = position;
+                // ListView Clicked item value
+                String itemValue = (String) lv.getItemAtPosition(position);
+                // Show Alert
+                Toast.makeText(getActivity(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+
+                //Intent intent = new Intent(getActivity(), fragment2.class);
+                //intent.putExtra("channel", itemValue);
+                //startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/edu/stamford/scitech/stiuvantracker/fragment2.java
+++ app/src/main/java/edu/stamford/scitech/stiuvantracker/fragment2.java
@@ -1,37 +1,60 @@
 package edu.stamford.scitech.stiuvantracker;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.pubnub.api.Callback;
+import com.pubnub.api.Pubnub;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class fragment2 extends Fragment implements OnMapReadyCallback {
 
-    private static final String TAG = "Tracker - GMaps Share";
+    private static final String TAG = "Tracker - GMaps Follow";
+    private static final String PUBNUB_TAG = "PUBNUB";
+    private boolean isFirstMessage = true;
     private boolean mRequestingLocationUpdates = false;
-    private MenuItem mShareButton;
-
-    // Google API - Locations
-    private GoogleApiClient mGoogleApiClient;
+    private Button viewButton;
+    private String buttonstate = "stop";
 
     // Google Maps
     private GoogleMap mGoogleMap;
+    //	private Polyline mPolyline;
     private PolylineOptions mPolylineOptions;
+    private Marker mMarker;
+    private MarkerOptions mMarkerOptions;
     private LatLng mLatLng;
 
+    // PubNub
+    private Pubnub mPubnub;
+    private String channelName;
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -68,6 +91,11 @@ public class fragment2 extends Fragment implements OnMapReadyCallback {
         map.addMarker(new MarkerOptions()
                 .position(new LatLng(10, 10))
                 .title("Hello world"));
+
+        mGoogleMap = map;
+        mGoogleMap.setMyLocationEnabled(true);
+        Log.d(TAG, "Map Ready");
+
     }
 
     @Override
@@ -79,6 +107,110 @@ public class fragment2 extends Fragment implements OnMapReadyCallback {
 
         SupportMapFragment mf = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.the_map);
         mf.getMapAsync(this);
+
+        // Get Channel Name
+        //Intent intent = getActivity().getIntent();
+        //channelName = intent.getExtras().getString("channel");
+        channelName = "Bansuan";
+        /*Log.d(TAG, "Passed Channel Name: " + channelName);
+        Toast.makeText(getActivity(), channelName,
+                Toast.LENGTH_LONG).show();
+
+        SupportMapFragment mf = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.the_map);
+        mf.getMapAsync(this);
+
+        viewButton = (Button) getActivity().findViewById(R.id.button_bc);
+        viewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRequestingLocationUpdates = !mRequestingLocationUpdates;
+                if (buttonstate == "stop" && mRequestingLocationUpdates) {
+                    buttonstate = "start";
+                    viewButton.setText("Stop Broadcast");
+                    startFollowingLocation();
+                } else if (buttonstate == "start" && !mRequestingLocationUpdates) {
+                    buttonstate = "stop";
+                    viewButton.setText("Start Broadcast");
+                    stopFollowingLocation();
+
+                }
+            }
+        });*/
+
     }
+
+    private void startFollowingLocation() {
+        initializePolyline();
+        mPubnub = PubNub.startPubnub();
+        PubNub.subscribe(mPubnub, channelName, subscribeCallback);
+    }
+
+    private void stopFollowingLocation() {
+        mPubnub.unsubscribe(channelName);
+        isFirstMessage = true;
+    }
+
+
+    // =========================================================================
+    // Map Editing Methods
+    // =========================================================================
+
+    private void initializePolyline() {
+        mGoogleMap.clear();
+        mPolylineOptions = new PolylineOptions();
+        mPolylineOptions.color(Color.BLUE).width(10);
+        mGoogleMap.addPolyline(mPolylineOptions);
+
+        mMarkerOptions = new MarkerOptions();
+    }
+
+    private void updatePolyline() {
+        mPolylineOptions.add(mLatLng);
+        mGoogleMap.clear();
+        mGoogleMap.addPolyline(mPolylineOptions);
+    }
+
+    private void updateCamera() {
+        mGoogleMap
+                .animateCamera(CameraUpdateFactory.newLatLngZoom(mLatLng, 16));
+    }
+
+    private void updateMarker() {
+//		if (!isFirstMessage) {
+//			isFirstMessage = false;
+//			mMarker.remove();
+//		}
+        mMarker = mGoogleMap.addMarker(mMarkerOptions.position(mLatLng));
+    }
+
+    // =========================================================================
+    // PubNub Callback
+    // =========================================================================
+
+    Callback subscribeCallback = new Callback() {
+
+        @Override
+        public void successCallback(String channel, Object message) {
+            Log.d(PUBNUB_TAG, "Message Received: " + message.toString());
+            JSONObject jsonMessage = (JSONObject) message;
+            try {
+                double mLat = jsonMessage.getDouble("lat");
+                double mLng = jsonMessage.getDouble("lng");
+                mLatLng = new LatLng(mLat, mLng);
+            } catch (JSONException e) {
+                Log.e(TAG, e.toString());
+            }
+
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    updatePolyline();
+                    updateCamera();
+                    updateMarker();
+                }
+            });
+        }
+    };
+
 }
 
